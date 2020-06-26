dd6f0dc419517eeeb025d82c8382865253a35a6b
LiquidEagle <46903561+LiquidEagle@users.noreply.github.com>
2020-06-26 13:22:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,5 @@ client.registry.registerGroups([
 
 client.on('ready', () => {
     console.log("Bot has logged in.");
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> bb6fd5292ddd5914dfc01dd8943f57fd5505aa93
+
